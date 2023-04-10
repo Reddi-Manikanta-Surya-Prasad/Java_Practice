0c0a8f00d00728b2099fbc725781202f61e30d28
Reddi Manikanta Surya Prasad <reddimani14@gmail.com>
2023-04-10 20:29:59 +0530

    .

---
+++ day2.java
@@ -0,0 +1,10 @@
+public class day2{
+    public static void main(String args[]) {
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+    }
+}
