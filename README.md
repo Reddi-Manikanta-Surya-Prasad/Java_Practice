42de7613ce3854380346fadd56f76be6674be01e
Reddi Manikanta Surya Prasad <reddimani14@gmail.com>
2023-04-10 21:14:47 +0530

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ACCIOJOB
+# First java Program --> printing Hello World
+# Single line comments using // This is a signle line comment
---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        /*
+         * This is a 
+         * Multiline comment
+         */
+        int age = 12; // declaring and initialising the age variable
+        
+        System.out.println(age);
+        }
+}
