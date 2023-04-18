fae632e9a0421870a8941e9c61984384d7b57de6
Reddi Manikanta Surya Prasad <reddimani14@gmail.com>
2023-04-18 23:45:48 +0530

    .

---
+++ DiamondPattern.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class DiamondPattern {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+            //breaking the pattern into 2 parts --> upper_part & lower_part
+            n = (n+1)/2;
+            //upper pattern
+            for(int i=1;i<=n;i++){
+                //1.n-i spaces
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=i;k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+
+            }
+
+            //lower pattern
+            for(int i=n-1;i>=1;i--){
+                //1.n-i spaces
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=i;k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+                
+            }
+        
+    }
+}
