80390d198e967a04ad61b7cb9454941fe7043503
Reddi Manikanta Surya Prasad <reddimani14@gmail.com>
2023-04-19 02:37:34 +0530

    .

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -10,10 +10,10 @@ public class DiamondPattern {
             for(int i=1;i<=n;i++){
                 //1.n-i spaces
                 for(int j=1;j<=n-i;j++){
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
-                for(int k=1;k<=i;k++){
-                    System.out.print("*");
+                for(int k=1;k<=2*i-1;k++){
+                    System.out.print("* ");
                 }
                 System.out.println();
 
@@ -23,10 +23,10 @@ public class DiamondPattern {
             for(int i=n-1;i>=1;i--){
                 //1.n-i spaces
                 for(int j=1;j<=n-i;j++){
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
-                for(int k=1;k<=i;k++){
-                    System.out.print("*");
+                for(int k=1;k<=2*i-1;k++){
+                    System.out.print("* ");
                 }
                 System.out.println();
                 
