fd8c7dbc1bf2fdb5fa7cda815472e5db9b58a742
Reddi Manikanta Surya Prasad <reddimani14@gmail.com>
2023-04-21 01:53:31 +0530

    .

--- Unary.java
+++ Unary.java
@@ -2,6 +2,13 @@ public class Unary {
     public static void main(String []args) {
         int a = 10;
         int b = 20;
-        System.out.println();
+        a++;
+        System.out.println("a increment: "+a);
+        a--;
+        System.out.println("a decrement: "+a);
+        b--;
+        System.out.println("b decrement: "+b);
+        b++;
+        System.out.println("b increment: "+b);
     }
 }
